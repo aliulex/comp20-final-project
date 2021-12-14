5eb096bd82b81d432f65746961a4bb88e32dedc7
Alex Liu <aliu280@gmail.com>
2021-12-14 00:06:20 -0500

    display from database should work

--- genius.js
+++ genius.js
@@ -2,8 +2,9 @@ import http from 'http';
 import fs from 'fs';
 import qs from 'querystring';
 import Genius from 'genius-lyrics';
-const Client = new Genius.Client("o19mtznUl0mu0kk0DZxZgATNB-2Cw0ihj5ybfCdOpd-pz25oY7A4J5BgGQEoqDGb");
 import Mongo from 'mongodb';
+
+const Client = new Genius.Client("o19mtznUl0mu0kk0DZxZgATNB-2Cw0ihj5ybfCdOpd-pz25oY7A4J5BgGQEoqDGb");
 const MongoClient = Mongo.MongoClient;
 const url = "mongodb+srv://aliulex:aliulex0@cluster0.qxqu8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; 
 
@@ -50,11 +51,7 @@ http.createServer(function (req, res)
 	        }
 			
 			const searches = await Client.songs.search(query);
-
-			// Pick first one
 			const firstSong = searches[0];
-
-			// Ok lets get the lyrics
 			lyrics = await firstSong.lyrics();
 			
 			res.write("<h1>Song Lyrics App</h1>");
@@ -72,35 +69,32 @@ http.createServer(function (req, res)
         
         MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
             if(err) { return console.log(err); return;}
-        
+            
             var dbo = db.db("favorites");
             var collection = dbo.collection('songs');
             var myobj = {song: query};
             
-            collection.insertOne(myobj, function(err, res) {
+            collection.insertOne(myobj, function(err, result) {
                 if(err) { console.log("query err: " + err); return; }
                 console.log("new document inserted");
             });
             
-            req.on('end', () => {
-                collection.find({}).toArray(function(err, items) {
-                    res.write("<h2>Your Favorite Songs:</h2>");
-                    if(items.length == 0) 
+            collection.find({}).toArray(function(err, items) {
+                res.write("<h2>Your Favorite Songs:</h2>");
+                if(items.length == 0) 
+                {
+                    res.write("No favorites");
+                }
+                else 
+                {
+                    for (let i=0; i<items.length; i++)
                     {
-                        res.write("No favorites");
+                        res.write(items[i].song + "<br><br>");
                     }
-                    else 
-                    {
-                        for (let i=0; i<items.length; i++)
-                        {
-                            res.write(items[i].song + "<br><br>");
-                        }
-                    } 
-                });
+                    res.end();
+                } 
             });
         
         });
-        
-        res.end();
     }
 }).listen(port); 
